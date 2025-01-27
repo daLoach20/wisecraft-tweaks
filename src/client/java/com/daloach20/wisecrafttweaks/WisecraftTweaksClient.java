c4e181b5788d9c978f739d31e884fe385f5d9f95
David Hamilton <8892995+daLoach20@users.noreply.github.com>
2025-01-27 07:21:41 -0600

    initial commit

---
+++ src/client/java/com/daloach20/wisecrafttweaks/WisecraftTweaksClient.java
@@ -0,0 +1,10 @@
+package com.daloach20.wisecrafttweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WisecraftTweaksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
